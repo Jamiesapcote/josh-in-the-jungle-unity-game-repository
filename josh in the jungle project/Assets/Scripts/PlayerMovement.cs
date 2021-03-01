eeacf89543d468f6c97070cb8c1b3cf597d9923f
Jamiesapcote <78355293+Jamiesapcote@users.noreply.github.com>
2021-03-01 13:00:31 +0000

    Player Movement code that we did in claas

---
+++ josh in the jungle project/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    //public variable visable in unity inspector
+    public float forceStrength;
+    //these will be called by each button for movment 
+    public void MoveLeft()
+    {
+        // get the rigidbody tjat we'll be using for movment 
+        Rigidbody2D OurRidgidbody = GetComponent<Rigidbody2D>();
+
+        //move in the correct direction with the set force strength 
+        OurRidgidbody.AddForce(Vector2.left * forceStrength);
+
+    }
+    public void MoveRight()
+    {
+        // get the rigidbody tjat we'll be using for movment 
+        Rigidbody2D OurRidgidbody = GetComponent<Rigidbody2D>();
+
+        //move in the correct direction with the set force strength 
+        OurRidgidbody.AddForce(Vector2.right * forceStrength);
+    }
+
+}
